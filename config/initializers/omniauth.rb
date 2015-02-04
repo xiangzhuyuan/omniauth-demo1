26fa6e11dc68c95d20840bdd4d64c506cf0b2f2b
xiang zhuyuan <xiangzhuyuan@gmail.com>
2015-02-04 20:12:50 +0800

    change the key to env

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '387319524698240', '5aa45dc530b0dd6c9946341986107399',
+  provider :facebook, ENV['F_KEY'], ENV['F_S'],
            :scope => 'email,user_birthday,read_stream', :display => 'popup'
-
-
-
-  provider :twitter, 'Et2ggoorIzUU1ZoqsL3w', '0sFz3JTKxoKwjErDADGL0LExDrBlwo3iWmhi4NM4bPE'
+  provider :twitter, ENV['T_KEY'], ENV['T_S']
 end
-#provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
